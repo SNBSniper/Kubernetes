a1d93ceb7e3adcae2e28ed7eca6528cb160d6190
Daniel Francisco Tapia Rybertt <danielf.tapiar@gmail.com>
2018-05-22 15:05:24 -0400

    added helm chart to user microservice and modified the delete_all.sh script

--- delete_all.sh
+++ delete_all.sh
@@ -1,4 +1,4 @@
-kubectl get pods --no-headers=true --all-namespaces |sed -r 's/(\S+)\s+(\S+).*/kubectl --namespace \1 delete pod \2/e'
-kubectl delete jobs,daemonsets,replicasets,services,deployments,pods --namespace default --all --grace-period=0 --force
+#kubectl get pods --no-headers=true --all-namespaces |sed -r 's/(\S+)\s+(\S+).*/kubectl --namespace \1 delete pod \2/e'
+kubectl delete jobs,daemonsets,replicasets,services,deployments,pods,ingress,secrets --namespace default --all --grace-period=0 --force
 
 
